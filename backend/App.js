67bf48bf12d6bb450d546e428e2d05bbe2968280
WJ-Bos <wjbosdev@gmail.com>
2024-08-16 13:56:12 +0200

    CI/CD Pipeline setup

---
+++ backend/App.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+//requests made to /message will receive this response.
+app.get("/message", (request, response) => {
+  response.json({ message: "This is message sent from the express server 👋" });
+});
+
+//have the Server listen om Port 5000
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    getMessage();
+  }, []);
+
+const getMessage = async () => {
+  try {
+    const response = await axios.get("http://localhost:5000/message");
+    setMessage(response.data.message); 
+  } catch (error) {
+    console.error('Error fetching message:', error);
+  }
+};
+
 
   return (
-    <>
+    <div>
+      <h1>Basic Server/Client App</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h3>Message from server</h3>
+        <div>
+          <p>{message}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
